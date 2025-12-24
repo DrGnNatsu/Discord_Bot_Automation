1ef3af84f3fea77724e3fe9cd5641bc43f1008e7
sondeptrys1tg <dnts0203@gmail.com>
2025-12-24 22:46:47 +0700

    Son | init login function

--- be_fastapi/app/main.py
+++ be_fastapi/app/main.py
@@ -2,12 +2,29 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from app.exception.base_exception import AppException
+
+@app.exception_handler(AppException)
+async def app_exception_handler(request: Request, exc: AppException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"message": exc.detail},
+    )
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+from app.api.v1.endpoints import auth_api
+
+
+
+
+app.include_router(auth_api.router)
+
+
+
